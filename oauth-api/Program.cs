934f3d6a01f0de20a71178d7df63ae2866cf33d7
André Paes França <afranca@compugraf.com.br>
2021-02-25 21:33:16 -0300

    criando autenticação

--- oauth-api/Program.cs
+++ oauth-api/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace oauth_api
 {
@@ -14,6 +9,10 @@ namespace oauth_api
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            var configuration = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .Build();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- oauth-api/Startup.cs
+++ oauth-api/Startup.cs
@@ -1,21 +1,16 @@
+using System.Text;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using oauth_service.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Update.Internal;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using Pomelo.EntityFrameworkCore.MySql.Internal;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace oauth_api
 {
@@ -31,14 +26,30 @@ namespace oauth_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "oauth_api", Version = "v1" });
             });
 
-            // Replace "YourDbContext" with the name of your own DbContext derived class.
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("Secret").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
             services.AddDbContextPool<AuthDbContext>(
                 dbContextOptions => dbContextOptions
                     .UseMySql(
@@ -49,11 +60,6 @@ namespace oauth_api
                     .EnableSensitiveDataLogging()
                     .EnableDetailedErrors()
             );
-
-            // services.AddDbContext<AuthDbContext>(
-            //     options =>
-            //         options.UseMySql(Configuration.GetConnectionString("AuthDB"),
-            //         x => x.MigrationsAssembly("oauth-service")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
